ded12099295b31992647cb7c4fbb2e2d8c1aa30c
reinoptland <reinoptland@gmail.com>
2019-10-18 14:49:03 +0200

    Add team details

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import TeamListContainer from './components/TeamListContainer'
-
+import TeamDetailsContainer from './components/TeamDetailsContainer'
 
 class App extends Component {
   render() {
@@ -14,7 +14,8 @@ class App extends Component {
           <Link to='/'> Home </Link>
           <Link to='/teams'> Teams </Link>
           <Route path="/" exact component={Home} />
-          <Route path="/teams" component={TeamListContainer} />
+          <Route path="/teams" exact component={TeamListContainer} />
+          <Route path="/teams/:id" exact component={TeamDetailsContainer} />
         </div>
       </Provider>
     );
---
+++ src/components/TeamDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TeamDetails(props){
+    if(!props.team.name) return 'Loading'
+
+    return <div>
+        {props.team.name}
+        <ul>
+            {props.team.players.map(player => {
+                return <li>{player.number} : {player.name}</li>
+            })}
+        </ul>
+    </div>
+}
+
+export default TeamDetails
