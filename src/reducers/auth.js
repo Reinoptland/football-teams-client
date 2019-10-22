167cb180b340272e889f36bf56baf15b663c1296
reinoptland <reinoptland@gmail.com>
2019-10-22 15:05:08 +0200

    Store token in localstorage, set as initialState

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,8 +1,12 @@
 import { LOGIN_SUCCESS } from "../actions/auth";
 
-export default (state = null, action = {}) => {
+const token = localStorage.getItem("jwt");
+const initialState = token ? token : null;
+
+export default (state = initialState, action = {}) => {
   switch (action.type) {
     case LOGIN_SUCCESS:
+      localStorage.setItem("jwt", action.payload);
       return action.payload;
     default:
       return state;
