e1d50e89676e912dcf0a0c7425f5271baae98ebd
reinoptland <reinoptland@gmail.com>
2019-10-22 15:15:37 +0200

    Redirect when we have a token in LoginForm

--- src/components/LoginFormContainer.js
+++ src/components/LoginFormContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import { connect } from "react-redux";
 import { login } from "../actions/auth";
+import { Redirect } from "react-router-dom";
 
 class LoginFormContainer extends React.Component {
   state = { email: "", password: "" };
@@ -9,6 +10,9 @@ class LoginFormContainer extends React.Component {
   onSubmit = event => {
     event.preventDefault();
     this.props.login(this.state.email, this.state.password);
+    // this.props.history.push('/') when redirecting like this,
+    // it will always happen if login failed
+    // Optimistic redirect
   };
 
   onChange = event => {
@@ -18,7 +22,10 @@ class LoginFormContainer extends React.Component {
   };
 
   render() {
-    return (
+    return this.props.token ? (
+      // If we have a token, redirect to Home
+      <Redirect to="/" />
+    ) : (
       <LoginForm
         onSubmit={this.onSubmit}
         onChange={this.onChange}
@@ -28,7 +35,13 @@ class LoginFormContainer extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    token: state.auth
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { login }
 )(LoginFormContainer);
