ea587038cd6ab7d45504dac56558499fa3a53bbb
reinoptland <reinoptland@gmail.com>
2019-10-22 14:41:14 +0200

    Add LoginForm Components & Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import TeamListContainer from "./components/TeamListContainer";
 import TeamDetailsContainer from "./components/TeamDetailsContainer";
+import LoginFormContainer from "./components/LoginFormContainer";
 
 class App extends Component {
   render() {
@@ -13,9 +14,11 @@ class App extends Component {
         <div>
           <Link to="/"> Home </Link>
           <Link to="/teams"> Teams </Link>
+          <Link to="/login"> Login</Link>
           <Route path="/" exact component={Home} />
           <Route path="/teams" exact component={TeamListContainer} />
           <Route path="/teams/:id" exact component={TeamDetailsContainer} />
+          <Route path="/login" exact component={LoginFormContainer} />
         </div>
       </Provider>
     );
